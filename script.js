f48c4eaa787b9a4ab15987421969a636a91ca8d2
jimmy <jimmynganga225@gmail.com>
2026-04-24 10:05:35 +0300

    fear: upgrade task to projects and add persistance

--- script.js
+++ script.js
@@ -16,3 +16,40 @@ function deletetask_(index) {
 // with:
 let todos = JSON.parse(localStorage.getItem("mytasks")) || [];
 // call rendertask() immidiatly so saved items on load rendertask();
+   // create a task object instead of just a straing
+   const newyask = {
+    text: tasktext,
+    completed: false
+    };
+
+    todos.push(newtask);
+    InputDeviceInpu.value = "";
+
+    rendertask();
+    savetolocalsstorage();
+function savetolocalsstorage() {
+    localStorage.setItem("mytasks", JSON.stringify(todos));
+}
+function toggleTaskCompletion(index) {
+    // this flips the compleated status(true/false)
+    todos[index].completed = !todos[index].completed;
+    rendertask();
+    savetolocalsstorage();
+}
+function rendertask() {
+    todolist.innerHTML = "";
+    todos.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.classlist = task.text;
+        if (task.completed) {
+            li.classList.add("completed");
+        }
+        li.innerHTML = `
+            <span>${task.text}</span>
+            <button onclick="deletetask_(${index})">Delete</button>
+        `;
+        todolist.appendChild(li);
+        // add click event to toggle completion
+        li.addEventListener("click", () => toggleTaskCompletion(index));
+    });
+}
